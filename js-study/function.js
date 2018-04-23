0544361110fb78fff295c602f425cb02b161ef44
1033096826 <1033096826@qq.com>
2018-04-23 17:37:38 +0800

    fun

--- js-study/function.js
+++ js-study/function.js
@@ -1,3 +1,16 @@
 /*
 函数学习
 * */
+function print(x) {
+    return console.log(x);
+}
+
+function abs() {
+    if (arguments.length === 0) {
+        return 0;
+    }
+    var x = arguments[0];
+    return x >= 0 ? x : -x;
+}
+
+print(abs(-10))
